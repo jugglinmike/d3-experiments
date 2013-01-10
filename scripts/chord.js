7be95649d0589b7ae5f7ede178c89b6f94d6a78d
Mike Pennisi <mike@mikepennisi.com>
2013-01-10 10:33:15 -0500

    Generalize definition of chord chart components
    Require instance-specific parameters to be supplied when the components
    are initialized.

--- scripts/chord.js
+++ scripts/chord.js
@@ -1,22 +1,24 @@
 (function(window, undefined) {
 
-function chord(options) {
+  var Chords = Rc.extend({
+    dataBind: function(data) {
+      return this.base.selectAll("path").data(data);
+    },
+    insert: function(entering) {
+      return entering.append("path");
+    },
+    events: {
+      enter: function(entering) {
+        var self = this;
 
-  var width = 960,
-      height = 500,
-      innerRadius = Math.min(width, height) * .41,
-      outerRadius = innerRadius * 1.1;
-  var svg = d3.select(options.container).append("svg")
-      .attr("width", width)
-      .attr("height", height)
-    .append("g")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-  var chordLayout = d3.layout.chord()
-      .padding(.05)
-      .sortSubgroups(d3.descending)
-  var fill = d3.scale.ordinal()
-      .domain(d3.range(4))
-      .range(["#000000", "#FFDD89", "#957244", "#F26223"]);
+        entering.attr("d", d3.svg.chord().radius(this.options.innerRadius))
+            .style("fill", function(d) {
+              return self.options.fill(d.target.index);
+            })
+            .style("opacity", 1);
+      }
+    }
+  });
 
   var Ticks = Rc.extend({
     dataBind: function(data) {
@@ -27,13 +29,14 @@ function chord(options) {
     },
     events: {
       enter: function(entering) {
+        var self = this;
         var tickGroup = entering.selectAll("g")
             .data(this.group)
           .enter().append("g");
 
         tickGroup.attr("transform", function(d) {
               return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
-                  + "translate(" + outerRadius + ",0)";
+                  + "translate(" + self.options.outerRadius + ",0)";
             });
 
         tickGroup.append("line")
@@ -75,9 +78,14 @@ function chord(options) {
     },
     events: {
       enter: function(entering) {
-        entering.style("fill", function(d) { return fill(d.index); })
-            .style("stroke", function(d) { return fill(d.index); })
-            .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius))
+        var self = this;
+        var path = d3.svg.arc()
+          .innerRadius(this.options.innerRadius)
+          .outerRadius(this.options.outerRadius);
+
+        entering.style("fill", function(d) { return self.options.fill(d.index); })
+            .style("stroke", function(d) { return self.options.fill(d.index); })
+            .attr("d", path)
             .on("mouseover", this.fade(.1))
             .on("mouseout", this.fade(1));
       }
@@ -93,25 +101,39 @@ function chord(options) {
     }
   });
 
-  var Chords = Rc.extend({
-    dataBind: function(data) {
-      return this.base.selectAll("path").data(data);
-    },
-    insert: function(entering) {
-      return entering.append("path");
-    },
-    events: {
-      enter: function(entering) {
-        entering.attr("d", d3.svg.chord().radius(innerRadius))
-            .style("fill", function(d) { return fill(d.target.index); })
-            .style("opacity", 1);
-      }
-    }
-  });
+function chord(options) {
 
-  var ticks = new Ticks({ base: svg.append("g").attr("class", "ticks") });
-  var handles = new Handles({ base: svg.append("g").attr("class", "events") });
-  var chords = new Chords({ base: svg.append("g").attr("class", "chords") });
+  var width = 960,
+      height = 500,
+      innerRadius = Math.min(width, height) * .41,
+      outerRadius = innerRadius * 1.1;
+  var svg = d3.select(options.container).append("svg")
+      .attr("width", width)
+      .attr("height", height)
+    .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+  var chordLayout = d3.layout.chord()
+      .padding(.05)
+      .sortSubgroups(d3.descending)
+  var fill = d3.scale.ordinal()
+      .domain(d3.range(4))
+      .range(["#000000", "#FFDD89", "#957244", "#F26223"]);
+
+  var ticks = new Ticks({
+    base: svg.append("g").attr("class", "ticks"),
+    outerRadius: outerRadius
+  });
+  var handles = new Handles({
+    base: svg.append("g").attr("class", "events"),
+    fill: fill,
+    innerRadius: innerRadius,
+    outerRadius: outerRadius
+  });
+  var chords = new Chords({
+    base: svg.append("g").attr("class", "chords"),
+    fill: fill,
+    innerRadius: innerRadius
+  });
 
   return function(matrix) {
 
