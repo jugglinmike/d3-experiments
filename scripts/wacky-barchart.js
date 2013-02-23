c60075501acf1a40d4669a37e131d128ffd94ff6
Mike Pennisi <mike@mikepennisi.com>
2013-02-22 19:19:16 -0500

    Further simplify mixin declaration
    Requiring a `namespace` string in mixin creation clutters the method
    signature and limits the user's options on how to attach the mixin chart
    (i.e. it can only be attached to the chart instance directly). This API
    leaves the method of reference attaching completely up to the user.
    
    Not sure if this is a good idea: the user may not keep references to the
    chart, which will be problematic if we later decide to implement more
    mixin management methods (like `removeMixin`, for example... ooo! We
    could call that `mixout`!)

--- scripts/wacky-barchart.js
+++ scripts/wacky-barchart.js
@@ -2,7 +2,7 @@ d3.chart("WackyBC", {
 
   initialize: function(options) {
 
-    this.mixin("bc", "BarChart", options);
+    this.bc = this.mixin("BarChart", options);
     this.base.attr("width", this.bc.base.attr("width"));
     this.base.attr("height", this.bc.base.attr("height"));
 
