6648e07533ced898ab28bca23f04e71ee83c587d
Mike Pennisi <mike@mikepennisi.com>
2013-02-22 20:24:40 -0500

    Actually implement selection-based chart creation
    Prior to this commit, chart "bases" needed to be specified as the first
    argument. Since the chart constructor has been moved to
    `d3.selection.prototype`, this is no longer necessary--the constructor
    can (and should) infer its base from the selection it was called on.
    Behing the scenes, all charts will expect the base selection as the
    first argument to their constructor. The end user does not have to be
    aware of this requirement, because the first argument can be omitted in
    the invocation of Chart#initialize.
    
    This work necessitated a more d3-like API for mixins. This is a bad
    idea and should be re-worked ASAP.

--- scripts/wacky-barchart.js
+++ scripts/wacky-barchart.js
@@ -2,7 +2,7 @@ d3.chart("WackyBC", {
 
   initialize: function(options) {
 
-    this.bc = this.mixin("BarChart", options);
+    this.bc = this.mixin(this.base.append("g"), "BarChart", options);
     this.base.attr("width", this.bc.base.attr("width"));
     this.base.attr("height", this.bc.base.attr("height"));
 
